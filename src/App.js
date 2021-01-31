0c5cb87f5abc06aa89dcdb006ba3dead5d9245e0
Roman Yarovoy <roman@booma.co.il>
2021-01-31 12:06:19 +0200

    add__rep_

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <ChatEngine
+        height="100vh"
+        projectID="5e886549-1d61-489e-b908-0def90988b5c"
+        userName="javascript"
+        userSecret="b005ab7b-228e-4249-99f7-40bba8883ef2"
+      />
+    </div>
+  );
+};
+export default App;
