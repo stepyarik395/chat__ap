33b83966744fa32407523f3c4f973cd15e551cab
Roman Yarovoy <roman@booma.co.il>
2021-02-04 10:06:00 +0200

    fix

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -30,11 +30,20 @@ const ChatFeed = ( props ) => {
       )
     })
   }
-  renderMessages()
+
+  if (!chat) return 'Loading ...'
   return ( 
     <div className="chat-feed">
       <div className="chat-title-container">
         <div className="chat-title">{chat?.title}</div>
+        <div className="chat-subtitle">
+          {chat.people.map((person) => ` ${person.person.username}`)}
+        </div>
+      </div>
+      {renderMessages()}
+      <div style={{ height: '100px' }} />
+      <div className="message-form-container">
+
       </div>
     </div>
   )
