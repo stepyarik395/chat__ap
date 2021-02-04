31219ed121aa12b6370ca8d096670b529aaf34b0
Roman Yarovoy <roman@booma.co.il>
2021-02-04 09:46:24 +0200

    fix

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,10 +1,42 @@
- const ChatFeed = (props) => {
-  console.log(props);
-  return (
-    <div>
-      ChatFeed
+import MessegeForm from "../components/MessegeForm" 
+import MyMessage from "../components/MyMessage" 
+import TheirMessage from "../components/TheirMessage" 
+
+
+const ChatFeed = ( props ) => {
+  const { chats, activeChat, userName, messages } = props
+  const chat = chats && chats[activeChat]
+
+  const renderMessages = () => {
+    const keys = Object.keys(messages)
+    return keys.map((key, index) => {
+      const messege = messages[key];
+      const lastMessageKey = index === 0 ? null : keys[index - 1]
+      const isMyMessege = userName === messages.sender.username;
+      return (
+        <div key={`msg_${index}`} style={{ width: '100%' }}>
+          <div className="message-block">
+            {
+              isMyMessege
+                ? <MyMessage />
+                :<TheirMessage />
+
+            }
+          </div>
+          <div className="red-receipts" style={{ marginRight: isMyMessege ? '18px' : '0px', marginLeft: isMyMessege ? '0px' : '68px' }}>
+          red-receipts
+          </div>
+        </div>
+      )
+    })
+  }
+  renderMessages()
+  return ( 
+    <div className="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title">{chat?.title}</div>
+      </div>
     </div>
   )
 }
-
 export default ChatFeed;
